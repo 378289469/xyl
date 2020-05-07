8b4d774be531567c30085dba665167e268fff1dd
xyl@gs.commm <xyl@gs.comm>
2020-05-07 17:49:30 +0800

    pdf

--- src/views/PDF/PDF.vue
+++ src/views/PDF/PDF.vue
@@ -6,7 +6,16 @@
     </Header>
     <div class="introduce">
       <div class="conternt">
-        <pdf :src="url"/>
+          <div class="tools">
+              <bk-button :theme="'default'" type="submit" :title="'基础按钮'" @click.stop="prePage" class="mr10"> 上一页</bk-button>
+              <bk-button :theme="'default'" type="submit" :title="'基础按钮'" @click.stop="nextPage" class="mr10"> 下一页</bk-button>
+              <div class="page">{{pageNum}}/{{pageTotalNum}} </div>
+              <bk-button :theme="'default'" type="submit" :title="'基础按钮'" @click.stop="clock" class="mr10"> 顺时针</bk-button>
+              <bk-button :theme="'default'" type="submit" :title="'基础按钮'" @click.stop="counterClock" class="mr10"> 逆时针</bk-button>
+          </div>
+          <pdf ref="pdf" :src="url" :page="pageNum" :rotate="pageRotate" @progress="loadedRatio = $event"
+          @page-loaded="pageLoaded($event)"  @num-pages="pageTotalNum=$event" @error="pdfError($event)"
+          @link-clicked="page = $event"></pdf>
       </div>
     </div>
   </div>
@@ -16,12 +25,20 @@
 import Header from '../../components/Header/Header'
 import { mapState } from 'vuex'
 import BScroll from 'better-scroll'
-import axios from 'axios'
 import pdf from 'vue-pdf'
 export default {
   data () {
     return {
-      url: ''
+      // url: 'http://storage.xuetangx.com/public_assets/xuetangx/PDF/PlayerAPI_v1.0.6.pdf',
+      url: 'http://192.168.5.56:8096//3c4ea9f890e8da152257fb74e6799bd6.pdf',
+      pageNum: 1,
+      pageTotalNum: 1,
+      pageRotate: 0,
+      // 加载进度
+      loadedRatio: 0,
+      curPageNum: 0,
+      dataType: 'jsonp',
+      crossDomain: true
     }
   },
   components: {
@@ -34,15 +51,38 @@ export default {
   methods: {
     back () {
       this.$router.go(-1)
+    },
+    // 上一页函数，
+    prePage () {
+      var page = this.pageNum
+      page = page > 1 ? page - 1 : this.pageTotalNum
+      this.pageNum = page
+    },
+    // 下一页函数
+    nextPage () {
+      var page = this.pageNum
+      page = page < this.pageTotalNum ? page + 1 : 1
+      this.pageNum = page
+    },
+    // 页面顺时针翻转90度。
+    clock () {
+      this.pageRotate += 90
+    },
+    // 页面逆时针翻转90度。
+    counterClock () {
+      this.pageRotate -= 90
+    },
+    // 页面加载回调函数，其中e为当前页数
+    pageLoaded (e) {
+      this.curPageNum = e
+    },
+    // 其他的一些回调函数。
+    pdfError (error) {
+      console.error(error)
     }
   },
   mounted () {
     new BScroll('.conternt') // eslint-disable-line
-    console.log('++++')
-    this.url = axios.get('http://192.168.5.56:8096//3c4ea9f890e8da152257fb74e6799bd6.pdf')
-  },
-  created () {
-    // this.url = pdf.createLoadingTask(this.url)
   }
 }
 </script>
