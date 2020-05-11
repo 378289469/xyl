ca1c7c576287e81dd8e68c60036c7810370f54cd
xyl@gs.commm <xyl@gs.comm>
2020-05-11 16:35:47 +0800

    活动详情页

--- src/views/ActivityDetail/ActivityDetail.vue
+++ src/views/ActivityDetail/ActivityDetail.vue
@@ -21,15 +21,17 @@
         <div class="media" v-if="ad.content === 2" >
           <viewer class="imgWrap" :images="PdfFile">
             <img v-for="(pdf, index) in PdfFile" :src="pdf.path" :key="index">
-          </viewer>
-          <video-player  class="video-player vjs-custom-skin"
-            ref="videoPlayer" 
-            :playsinline="true" 
+            <video-player  class="video-player vjs-custom-skin"
+            ref="videoPlayer"
+            :playsinline="true"
             :options="playerOptions"
-          ></video-player>
+            ></video-player>
+          </viewer>
         </div>
       </div>
-      <button>提问</button>
+      <button @click="wheel('提问')">提问</button>
+      <wheel ref="wheel"/>
+      <tip/>
     </div>
   </div>
 </template>
@@ -38,6 +40,8 @@
 import { mapState } from 'vuex'
 import BScroll from 'better-scroll'
 import Header from '../../components/Header/Header'
+import wheel from '../../components/Wheel/wheel'
+import tip from '../../components/Tip/tip'
 
 export default {
   data () {
@@ -48,30 +52,32 @@ export default {
         autoplay: false, // 如果true,浏览器准备好时开始回放。
         muted: false, // 默认情况下将会消除任何音频。
         loop: false, // 导致视频一结束就重新开始。
-        preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-        language: "zh-CN",
-        aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        language: 'zh-CN',
+        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
         fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
         sources: [
           {
-            type: "video/mp4", // 这里的种类支持很多种：基本视频格式、直播、流媒体等，具体可以参看git网址项目
-            src: require("@/assets/vedio.mp4") // url地址
+            type: 'video/mp4', // 这里的种类支持很多种：基本视频格式、直播、流媒体等，具体可以参看git网址项目
+            src: '1.mp4' // url地址
           }
         ],
-        poster: require("@/assets/poster.png"), // 你的封面地址
+        poster: '1.jpg', // 你的封面地址
         // width: document.documentElement.clientWidth, //播放器宽度
-        notSupportedMessage: "此视频暂无法播放，请稍后再试", // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
+        notSupportedMessage: '此视频暂无法播放，请稍后再试', // 允许覆盖Video.js无法播放媒体源时显示的默认信息。
         controlBar: {
           timeDivider: true,
           durationDisplay: true,
           remainingTimeDisplay: false,
           fullscreenToggle: true // 全屏按钮
         }
-      },
+      }
     }
   },
   components: {
-    Header
+    Header,
+    wheel,
+    tip
   },
   computed: {
     activity () {
@@ -102,6 +108,9 @@ export default {
     },
     go () {
       this.Isclick = !this.Isclick
+    },
+    wheel (title) {
+      this.$refs.wheel.wheel(title, this.activity.id)
     }
   },
   mounted () {
