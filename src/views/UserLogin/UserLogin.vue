8d130d9055e1b09dda90c6b4f1f0a4a0335b46d8
xyl@gs.commm <xyl@gs.comm>
2020-05-13 13:41:31 +0800

    用户页

--- src/views/UserLogin/UserLogin.vue
+++ src/views/UserLogin/UserLogin.vue
@@ -5,9 +5,12 @@
     </Header>
     <form action="" class="items">
       <h2>用户登录</h2>
-      <div class="item">
-        <input :type="types[i]" :placeholder="placeholders[i]"  v-for="i in 3" :key="i">
+      <div class="item"  v-for="i in 2" :key="i">
+        <span class="iconfont" :class="iconClass[i]"></span>
+        <input :type="types[i]" :placeholder="placeholders[i]" >
       </div>
+      <button>登录</button>
+      <h3><span>还没有账号？</span>立即注册</h3>
     </form>
   </div>
 </template>
@@ -19,8 +22,9 @@ import Header from '../../components/Header/Header'
 export default {
   data () {
     return {
-      types: ['', 'text', 'text', 'submit'],
-      placeholders: ['', '请输入用户名', '请输入密码', '']
+      types: ['', 'text', 'text'],
+      placeholders: ['', '请输入用户名', '请输入密码', ''],
+      iconClass: ['', 'icon-username', 'icon-userpw']
     }
   },
   components: {
@@ -52,10 +56,41 @@ export default {
       font-weight bolder
       margin-top 63px
     .item
+      display flex
+      justify-content center
       width 80%
       height 45px
       border-radius 22px
       background-color white
       border 1px solid #B39845
+      margin auto
       margin-top 13px
+      span
+        display block
+        width 13px
+        height 13px
+        color #A37257
+        margin-top 14px
+      input
+        width 200px
+        height 20px
+        margin-top 12px
+        margin-left 10px
+        color #828282
+        font-size 14px
+        border none
+    button
+      margin-top 25px
+      color #FFF6C1
+      width 85%
+      height 45px
+      border-radius 22px
+      background-color #6C0B0D
+    h3
+      margin-top 15px
+      font-size 13px
+      color #B69B4B
+      span
+        color #DB1E22
+        margin-right 5px
 </style>
--- src/views/UserRegister/UserRegister.vue
+++ src/views/UserRegister/UserRegister.vue
@@ -1,42 +1,33 @@
 <template>
-  <div id="my">
+  <div id="UserLogin">
     <Header>
       <img src="./imgs/title.png" alt="title" class="title" slot="title">
     </Header>
-    <Info/>
-    <div class="item">
-      <List v-for="i in 2" :key="i" @click.native="to(i)">
-          <span :class="leftClass[i]" slot="left"></span>
-          <span class="title" slot="title">{{titles[i]}}</span>
-          <span class="iconfont icon-right" slot="right"></span>
-      </List>
-    </div>
+    <form action="" class="items">
+      <h2>用户注册</h2>
+      <div class="item"  v-for="i in 4" :key="i">
+        <span class="iconfont" :class="iconClass[i]"></span>
+        <input :type="types[i]" :placeholder="placeholders[i]" >
+      </div>
+      <button>注册</button>
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from '../../components/Header/Header'
-import Info from '../../components/My/Info/Info.vue'
-import List from '../../components/My/List/List.vue'
 
 export default {
   data () {
     return {
-      titles: ['', '小组成员', '账户安全'],
-      leftClass: ['', 'member', 'security'],
-      paths: ['', '/MyMember', '/MySecurity']
+      types: ['', 'text', 'text', 'text', 'text'],
+      placeholders: ['', '请输入姓名', '请输入手机号', '请输入密码', '请输入确认密码'],
+      iconClass: ['', 'icon-username', 'icon-phone', 'icon-userpw', 'icon-userpw']
     }
   },
   components: {
-    Header,
-    Info,
-    List
-  },
-  methods: {
-    to (index) {
-      this.$router.push(this.paths[index])
-    }
+    Header
   },
   mounted () {
   }
@@ -45,13 +36,60 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-#my
+#UserLogin
   width 100%
   height 100%
-  .item
+  .items
     position absolute
-    top 265px
-    left 14px
-    width 345px
+    top 97px
+    left 30px
+    width 315px
+    height 436px
     text-align center
+    background url('./imgs/loginbg.png') no-repeat
+    background-size 315px 436px
+    background-position center
+    h2
+      color #7C4900
+      font-size 22px
+      font-weight bolder
+      margin-top 55px
+    .item
+      display flex
+      justify-content center
+      width 80%
+      height 45px
+      border-radius 22px
+      background-color white
+      border 1px solid #B39845
+      margin auto
+      margin-top 13px
+      span
+        display block
+        width 13px
+        height 13px
+        color #A37257
+        margin-top 14px
+      input
+        width 200px
+        height 20px
+        margin-top 12px
+        margin-left 10px
+        color #828282
+        font-size 14px
+        border none
+    button
+      margin-top 25px
+      color #FFF6C1
+      width 85%
+      height 45px
+      border-radius 22px
+      background-color #6C0B0D
+    h3
+      margin-top 15px
+      font-size 13px
+      color #B69B4B
+      span
+        color #DB1E22
+        margin-right 5px
 </style>
